1100a6c07725c055031d15ea918a36901535e6b2
Jaqueline Poletto <poletto.jaque@gmail.com>
2021-10-19 20:32:32 -0300

    app-web-backend
    Aula https://pucminas.instructure.com/courses/68749/pages/unidade-2-tema-1-ambiente-de-desenvolvimento?module_item_id=1517421

---
+++ app-web-backend/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * UNIDADE 2 - Tema 1 - Ambiente de Desenvolvimento
+ * https://pucminas.instructure.com/courses/68749/pages/unidade-2-tema-1-ambiente-de-desenvolvimento?module_item_id=1517421
+ */
+
+namespace app_web_backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
