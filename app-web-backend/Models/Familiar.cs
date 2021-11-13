80c7ca833eeda974f6d4142b3620b598f894ff64
JaquelinePoletto <poletto.jaque@gmail.com>
2021-11-13 20:41:02 -0300

    Create Familiar.cs

---
+++ app-web-backend/Models/Familiar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_web_backend.Models
+{
+    [Table("Familiares")]
+    public class Familiar
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "*Campo Obrigatório")]
+        public string Nome { get; set; }
+    }
+}
+
